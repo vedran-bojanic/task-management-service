703f9f9d2379a75a2cab7637f297dd6335a99128
Vedran Bojanić <v.bojanic@reply.de>
2025-01-01 11:40:48 +0100

    ASEE-004 Fetch task by id flow tests

--- src/test/java/com/asee/taskmanagementservice/task/controller/TaskManagementControllerTest.java
+++ src/test/java/com/asee/taskmanagementservice/task/controller/TaskManagementControllerTest.java
@@ -1,10 +1,13 @@
 package com.asee.taskmanagementservice.task.controller;
 
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.asee.taskmanagementservice.task.model.TaskDTO;
+import com.asee.taskmanagementservice.task.service.TaskManagementService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
@@ -12,20 +15,56 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 class TaskManagementControllerTest {
 
+    @Autowired MockMvc mockMvc;
+    @Autowired ObjectMapper objectMapper;
+
+    @MockitoBean
+    private TaskManagementService taskManagementService;
+
     @Test
-    void should_returnStatusOK_when_creatingTask(
-        @Autowired MockMvc mockMvc,
-        @Autowired ObjectMapper objectMapper
-    ) throws Exception {
+    void should_returnStatusOK_when_creatingTask() throws Exception {
         // prepare data
         TaskDTO taskDTO = new TaskDTO(
-            null,
+            1,
+            "name",
+            "description",
+            "CREATED",
+            "2024-12-31T10:00:00+01:00[Europe/Zagreb]",
+            "2024-12-31T10:00:00+01:00[Europe/Zagreb]",
+            1,
+            "testUser");
+
+        // Mock the service response
+        when(taskManagementService.create(taskDTO)).thenReturn(taskDTO);
+
+        // call endpoint
+        mockMvc.perform(post("/tasks")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(taskDTO)))
+            .andExpect(status().isCreated())
+            .andExpect(jsonPath("$", Matchers.notNullValue()))
+            .andExpect(jsonPath("$.id", Matchers.is(taskDTO.id())))
+            .andExpect(jsonPath("$.name", Matchers.is(taskDTO.name())))
+            .andExpect(jsonPath("$.description", Matchers.is(taskDTO.description())))
+            .andExpect(jsonPath("$.status", Matchers.is(taskDTO.status())))
+            .andExpect(jsonPath("$.createdOn", Matchers.is(taskDTO.createdOn())))
+            .andExpect(jsonPath("$.dueDate", Matchers.is(taskDTO.dueDate())))
+            .andExpect(jsonPath("$.userId", Matchers.is(taskDTO.userId())))
+            .andExpect(jsonPath("$.username", Matchers.is(taskDTO.username())));
+    }
+
+    @Test
+    void should_returnNotFound_when_fetchingTaskById() throws Exception {
+        // prepare data
+        TaskDTO taskDTO = new TaskDTO(
+            1,
             "name",
             "description",
             "CREATED",
@@ -34,11 +73,33 @@ class TaskManagementControllerTest {
             null,
             null);
 
+        // Mock the service response
+        when(taskManagementService.getTaskById(taskDTO.id())).thenReturn(null);
+
         // call endpoint
-        mockMvc.perform(post("/tasks")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(taskDTO)))
-            .andExpect(status().isCreated())
+        mockMvc.perform(get("/tasks/" + taskDTO.id()).contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void should_returnTaskDTO_when_fetchingTaskById() throws Exception {
+        // prepare data
+        TaskDTO taskDTO = new TaskDTO(
+            1,
+            "name",
+            "description",
+            "CREATED",
+            "2024-12-31T10:00:00+01:00[Europe/Zagreb]",
+            "2024-12-31T10:00:00+01:00[Europe/Zagreb]",
+            null,
+            null);
+
+        // Mock the service response
+        when(taskManagementService.getTaskById(1)).thenReturn(taskDTO);
+
+        // call endpoint
+        mockMvc.perform(get("/tasks/" + taskDTO.id()).contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
             .andExpect(jsonPath("$", Matchers.notNullValue()))
             .andExpect(jsonPath("$.name", Matchers.is(taskDTO.name())))
             .andExpect(jsonPath("$.description", Matchers.is(taskDTO.description())))
@@ -46,5 +107,4 @@ class TaskManagementControllerTest {
             .andExpect(jsonPath("$.createdOn", Matchers.is(taskDTO.createdOn())))
             .andExpect(jsonPath("$.dueDate", Matchers.is(taskDTO.dueDate())));
     }
-
 }
