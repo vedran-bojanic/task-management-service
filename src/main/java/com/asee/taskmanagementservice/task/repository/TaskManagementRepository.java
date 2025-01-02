2bbeec377e9d6462e5fe7c729eabc37d73dcb491
Vedran Bojanić <v.bojanic@reply.de>
2025-01-02 12:40:23 +0100

    ASEE-008 Get tasks by status workflow

--- src/main/java/com/asee/taskmanagementservice/task/repository/TaskManagementRepository.java
+++ src/main/java/com/asee/taskmanagementservice/task/repository/TaskManagementRepository.java
@@ -1,9 +1,11 @@
 package com.asee.taskmanagementservice.task.repository;
 
+import com.asee.taskmanagementservice.task.model.Status;
 import com.asee.taskmanagementservice.task.model.TaskEntity;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TaskManagementRepository extends JpaRepository<TaskEntity, Integer> {
     List<TaskEntity> findByUserId(Integer userId);
+    List<TaskEntity> findTaskEntityByStatus(Status status);
 }
